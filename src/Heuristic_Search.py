739cd49f3ec38152f3efd827bb0cf91f31b2064f
ArthurDornelas <arthurdornelas99@gmail.com>
2019-10-30 15:22:44 -0300

    Implementacao Heuristic Search

--- src/Heuristic_Search.py
+++ src/Heuristic_Search.py
@@ -1,6 +1,7 @@
 from src.Player import Player
 from src.Node import Node
 from src.Search import Search
+import math
 
 class HeuristicSearch(Search):
 
@@ -8,8 +9,19 @@ class HeuristicSearch(Search):
         super().__init__(file_path)
 
     def calculate_cost(self, node):
-        father = node.get_father()
-        node.set_heuristic_cost(father.get_total_cost() + node)
+        if node.get_father() is not None:
+            father = node.get_father()
+            node.set_real_cost(father.get_real_cost() + 1)
+        heuristic_cost = self.calculate_heuristic(node)
+        print("A heuristia eh: {}".format(heuristic_cost))
+        node.set_heuristic_cost(heuristic_cost)
+        node.set_total_cost(heuristic_cost)
 
     def add_to_opened_list(self, node):
         self.opened_list.append(node)
+
+    def calculate_heuristic(self, node):
+        point_node = node.get_position()
+        point_end_node = self.maze.get_ending()
+        distance = math.sqrt(((point_node[0] - point_end_node[0]) ** 2.0) + ((point_node[1] - point_end_node[1]) ** 2.0))
+        return distance
