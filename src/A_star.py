f242d7afb1b903249aaf9d5bae1c11d52958451c
Arthur Dornelas <arthurdornelas99@gmail.com>
2019-10-30 16:34:24 -0300

    Correcao AStar

--- src/A_star.py
+++ src/A_star.py
@@ -2,7 +2,8 @@ from abc import ABC
 from src.Search import Search
 import math
 
-class AStar(Search):
+
+class AStar(Search, ABC):
 
     def __init__(self, file_path):
         super().__init__(file_path)
@@ -11,22 +12,12 @@ class AStar(Search):
         if node.get_father() is not None:
             father = node.get_father()
             node.set_real_cost(father.get_real_cost() + 1)
-<<<<<<< HEAD
         heuristic_cost = self.calculate_heuristic(node)
         node.set_heuristic_cost(heuristic_cost)
         node.set_total_cost(node.get_real_cost() + node.get_heuristic_cost())
-=======
-            heuristic_cost = self.calculate_heuristic(node)
-            node.set_heuristic_cost(0)
-            node.set_total_cost(node.get_real_cost() + node.get_heuristic_cost())
->>>>>>> 6a5e2f0bd8a220287a7356bedcf65bca3315c99b
 
     def calculate_heuristic(self, node):
         point_node = node.get_position()
         point_end_node = self.maze.get_ending()
         distance = math.sqrt(((point_node[0] - point_end_node[0]) ** 2.0) + ((point_node[1] - point_end_node[1]) ** 2.0))
-<<<<<<< HEAD
         return distance
-=======
-        return distance
->>>>>>> 6a5e2f0bd8a220287a7356bedcf65bca3315c99b
